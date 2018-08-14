61ee762c25ea23dba763b94923279cc15fcf95a9
Rahul Chakravorty <jaiswal.anshuman@tftus.com>
2018-08-14 19:04:17 +0530

    user route

--- index.js
+++ index.js
@@ -8,19 +8,13 @@ var http = require('http');
 var https = require('https');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./config');
+var config = require('./lib/config');
 var fs = require('fs');
-var _data = require('./lib/data');
-
-// Testing
-// @TODO delete this
-_data.create('test','newFile', {'foo': 'bar'}, function(err) {
-   console.log('this was the error', err);
-});
+var handlers = require('./lib/handlers');
+var helpers = require('./lib/helpers');
 
 
-
-// Instantiating the HTTp server
+// Instantiating the HTTP server
 var httpServer = http.createServer(function(req, res){
     unifiedServer(req, res);
 });
@@ -83,7 +77,7 @@ var unifiedServer = function(req, res) {
             'queryStringObject': queryStringObject,
             'method': method,
             'headers': headers,
-            'payload': buffer
+            'payload': helpers.parseJsonToObject(buffer)
         };
         // route the request to the handler specified in the router
         chosenHandler(data, function(statusCode, payload){
@@ -110,21 +104,11 @@ var unifiedServer = function(req, res) {
     });
 };
 
-//Define the handlers
-var handlers = {};
-
-// Ping handler
-handlers.ping = function(data, callback) {
-    callback(200);
-}
-//Not found handler
-handlers.notFound = function(data, callback) {
-    callback(404);
-};
 
 //Define a request router
 var router = {
-    'ping' : handlers.ping
+    'ping' : handlers.ping,
+    'users': handlers.users
 }
 
 

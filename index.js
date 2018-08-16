b52e8ad75df2d6454f59b9cb27bb3d2e6da0de97
Rahul Chakravorty <jaiswal.anshuman@tftus.com>
2018-08-16 20:32:08 +0530

    token update

--- index.js
+++ index.js
@@ -108,7 +108,8 @@ var unifiedServer = function(req, res) {
 //Define a request router
 var router = {
     'ping' : handlers.ping,
-    'users': handlers.users
+    'users': handlers.users,
+    'tokens': handlers.tokens
 }
 
 
